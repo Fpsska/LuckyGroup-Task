99c91306e85b1ad9fa8fb1295e1c96f56347e34f
Fpsska <fpsska1337@gmail.com>
2021-06-12 11:44:49 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+const { src, dest, watch, parallel, series } = require("gulp");
+
+const scss = require("gulp-sass"); // минификация
+const concat = require("gulp-concat"); // конкатенация + имя
+const browserSync = require("browser-sync").create(); // live update 
+const uglify = require("gulp-uglify-es").default; //
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin"); // отпимизация img
+const del = require("del"); // удаление dist-папки
+// const ghpages = require('gh-pages'); // gh-pages for dist folder
+
+// ghpages.publish('dist', {
+//     repo: 'https://github.com/Fpsska/LuckyGroup-Task.git', //npm run deploy
+//     message: 'Auto-generated commit'
+// });
+
+
+function cleanDist() {
+    return del("dist");
+}
+
+function images() {
+    return src("app/img/**/*")
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]
+        ))
+        .pipe(dest("dist/img"));
+}
+
+function scripts() {
+    return src([
+        "app/js/main.js"
+    ])
+        .pipe(concat("main.min.js")) // конкатенация + единое название 
+        .pipe(uglify())   // минификация
+        .pipe(dest("app/js"))  // конечный путь
+        .pipe(browserSync.stream());
+}
+
+function styles() {       /*КОМПИЛЯЦИЯ scss -> style.min.css*/
+    return src([
+        "node_modules/normalize.css/normalize.css",
+        "app/scss/style.scss"
+    ]) 
+        .pipe(scss({ outputStyle: "compressed" })) // минификация
+        .pipe(concat("style.min.css")) // конкатенация + единое название  
+        .pipe(autoprefixer({
+            overrideBrowserslist: ["last 10 version"],
+            grid: true
+        }))
+        .pipe(dest("app/css")) // выкидывает в app/css
+        .pipe(browserSync.stream());
+}
+
+function browsersync() {  // live update 
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+function watching() { 
+    watch(["app/scss/**/*.scss"], styles);
+    watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+    watch(["app/*.html"]).on("change", browserSync.reload);
+}
+
+function build() {
+    return src([
+        "app/*.html",
+        "app/css/style.min.css",
+        "app/js/main.min.js",
+        "app/fonts/**/*", 
+    ], { base: "app" })
+        .pipe(dest("dist"));
+}
+
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles, scripts, browsersync, watching);
