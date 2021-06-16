af8ad0f54a54ff51b6ed3a58162db9d90a1f3fc4
Fpsska <fpsska1337@gmail.com>
2021-06-16 12:16:51 +0300

    fix wrong property in js & change opacity for inputs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 let phoneINP = document.querySelector('.input__tel');
 let inputMask = new Inputmask('+7 (999) 999-99-99');
 inputMask.mask(phoneINP);
-inputMask.style.fontsize="16px";
-inputMask.style.opacity="0.4";
+phoneINP.style.fontsize="16px";
+phoneINP.style.opacity="0.6";
 // // /. inputmask
